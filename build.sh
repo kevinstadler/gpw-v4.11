452f42d9f850a848c2ccb34b3ef4d0fdb96ffa82
shared <lionandoil+github2@gmail.com>
2020-10-22 14:05:57 +0800

    Add near-lossless webp tiles

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@
 # G     XXXX        XXXX     & 11110000 = 240
 # B         XXXX        XXXX left shift by 4
 
-if [ ! -f "interleaved.tif" ]; then
+if [ ! -f "PRE0-3POST0-3.tif" ]; then
   echo "Re-encoding data into 24 interleaved bits"
   # FIXME post-decimals?
 #    --calc="bitwise_or(bitwise_and(248, right_shift(A.astype(int), 7)), bitwise_and(7, (8*A).astype(int)))" \
@@ -25,22 +25,48 @@ if [ ! -f "interleaved.tif" ]; then
   gdal_calc.py --co="COMPRESS=LZW" --co="TFW=YES" --co PREDICTOR=2 --type=Byte --quiet \
     --calc="bitwise_or(left_shift(A.astype(int), 4), bitwise_and(15, (4096*A).astype(int)))" \
     --outfile="PRE8-11POST8-11.tif" --NoDataValue=0 -A "gpw_v4_population_density_rev11_2020_30_sec.tif"
+fi
 
+if [ ! -f "interleaved.tif" ]; then
   gdal_merge.py -co "COMPRESS=LZW" -co "TFW=YES" -co PREDICTOR=2 -ot Byte -separate -o "interleaved.tif" "PRE0-3POST0-3.tif" "PRE4-7POST4-7.tif" "PRE8-11POST8-11.tif"
 fi
 
-for PROFILE in geodetic mercator; do
-  mkdir "$PROFILE"
-  cd "$PROFILE"
+for PROFILE in mercator; do
+
   if [ ! -d "png" ]; then
     echo "Creating PNG tiles"
+    # mercator zoom level really only goes to 5, but build 6 as well for preserving equator high resolution
     # https://pypi.org/project/gdal2tiles/
     python3 <<< "import gdal2tiles
-options = { 'profile': '$PROFILE', 's_srs': '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', 'tile_size': 512, 'zoom': (1, 7), 'kml': False, 'webviewer': 'none' }
-gdal2tiles.generate_tiles('../interleaved.tif', 'png/', **options)"
+options = { 'profile': '$PROFILE', 's_srs': '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs', 'zoom': (1, 6), 'resume': True, 'tile_size': 512, 'kml': False, 'webviewer': 'none' }
+gdal2tiles.generate_tiles('interleaved.tif', 'png/', **options)"
   fi
 
-  cd "png"
+  cd png
+
+  DIRS=`ls -d */*`
+  for DIR in webpnl webp6; do
+    mkdir "../$DIR"
+    cd "../$DIR"
+    mkdir -p $DIRS
+  done
+
+  cd ../png
+
+  # mogrify is faster but with mercator level zoom 7 and upwards there are too many tile files to pass to mogrify as args...
+  for FILE in `ls */*/*.png`; do
+#    cwebp -short -z 6 $FILE -o "../webp6/`dirname $FILE`/`basename $FILE .png`.webp"
+    # even 100% quality lossy webp compression leads to artefacts because of how values are spread across the 3 channels (also at 4 bit)
+    #cwebp -short -q 100 $FILE -o "../webpl/`dirname $FILE`/`basename $FILE .png`.webp"
+    cwebp -short -near_lossless 60 $FILE -o "../webpnl/`dirname $FILE`/`basename $FILE .png`.webp"
+  done
+
+#  mogrify -format jp2 -quality 0 */*/*.png && rsync -rv --include '*/' --include '*.jp2' --exclude '*' --remove-source-files . ../jp2
+
+#  if [ ! -d "../webpl" ]; then
+#    echo "Creating lossless WebP tiles"
+#    mogrify -format webp -define webp:lossless=true */*/*.png && rsync -rv --include '*/' --include '*.webp' --exclude '*' --remove-source-files . ../webpl
+#  fi
 
   # if [ ! -d "../j" ]; then
   #   echo "Creating JPG 100 quality tiles"
@@ -50,10 +76,7 @@ gdal2tiles.generate_tiles('../interleaved.tif', 'png/', **options)"
 #    echo "Creating WebP tiles"
 #    mogrify -format webp            */*/*.png && rsync -rv --include '*/' --include '*.webp' --exclude '*' --remove-source-files . ../webp
 #  fi
-  if [ ! -d "../webpl" ]; then
-    echo "Creating lossless WebP tiles"
-    mogrify -format webp -define webp:lossless=true */*/*.png && rsync -rv --include '*/' --include '*.webp' --exclude '*' --remove-source-files . ../webpl
-  fi
 
-  cd ../..
+  cd ..
+
 done
